d74632394477542b30483a252f56e56de17d352d
Anoosragh <ok.ano0s6090@gmail.com>
2024-12-15 16:26:48 +0000

    hanoip: prebuilt: Include only custom logo
    Signed-off-by: Anoosragh <ok.ano0s6090@gmail.com>

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,20 +1,2 @@
 AB_OTA_PARTITIONS += \
-    abl \
-    aop \
-    bluetooth \
-    cmnlib \
-    cmnlib64 \
-    devcfg \
-    dsp \
-    fsg \
-    hyp \
-    keymaster \
-    logo \
-    modem \
-    prov \
-    qupfw \
-    storsec \
-    tz \
-    uefisecapp \
-    xbl \
-    xbl_config
+    logo
